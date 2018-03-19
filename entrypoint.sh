bd0eccaf085bab15a2f96b6f85fc0f4940a875de
fgassert <cowbox314@gmail.com>
2018-03-19 17:18:20 -0400

    remove read env and cred.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export $(cat .env)
-
 NRT_REPO=${TARGET_REPO:-https://github.com/resource-watch/nrt-scripts.git}
 NRT_DIR=$(basename $NRT_REPO .git)
 LOG=udp://logs6.papertrailapp.com:37123
@@ -21,7 +19,6 @@ echo "Creating cronfile"
 rm -f crontab
 find . -name "*.cron" | while read fname; do
     cp -f ../.env $(basename $(dirname $fname))
-    cp -f ../credentials.json $(basename $(dirname $fname))
     echo "$(cat $fname) cd $(pwd)/$(basename $(dirname $fname)) && LOG=$LOG ./start.sh >> /var/log/cron 2>&1" >> cron.tmp
 done
 
